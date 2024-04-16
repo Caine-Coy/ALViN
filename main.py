c8f9471f488708c34f1aeb03726a9af5c7d6ecf4
Joe <joe.benson0000@gmail.com>
2024-04-16 20:23:32 +0100

    Added Database Check

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from bleak import BleakScanner
 
 #Database URL
 POCKETBASE_URL = 'http://192.168.1.168:8080/api'
+BEACON_URL = '/collections/beacons/records'
+DEVICE_URL = '/collections/devices/records'
 DEVICE_NAME = socket.gethostname()
 
 async def update_device():
@@ -23,13 +25,26 @@ async def update_device():
 async def scan_and_update():
     devices = await BleakScanner.discover()
     localBeacons = [{'name': d.name, 'rssi': d.rssi} for d in devices]
-    print(localBeacons)
+    #print(localBeacons)
      #await update_device(beacons)
+
+
+def get_beacons_from_pocketbase():
+    response = requests.get(POCKETBASE_URL + BEACON_URL)
+    if response.ok:
+        beacons = response.json().get('items', [])
+        return beacons
+    else:
+        print(f"Failed to retrieve beacons: {response.content}")
+        return []
+
 
 async def main():
     while True:
         await scan_and_update()
+        print(get_beacons_from_pocketbase())
         await asyncio.sleep(60)  # Delay for 60 seconds
+        
 
 if __name__ == "__main__":
     asyncio.run(main())
